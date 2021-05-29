52530fb62048f0692cb1a194d42f277d166707b0
jimmy <jimmy.lin@stonybrook.edu>
2021-05-28 20:37:29 -0400

    Added map and left panel.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+import ReactMapGL, { Source, Layer } from "react-map-gl";
+import React, { useState, useContext, useEffect } from "react";
+
+const App = () => {
+  const [viewport, setViewport] = useState({
+    //set viewing settings for map
+    latitude: 40.730610,
+    longitude: -73.935242,
+    zoom: 10,
+    bearing: 0,
+    pitch: 0,
+  });
+
+  const processRegionSelection = (e) => {
+    console.log(e.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid"
+      style={{ height: "100vh", width: "100vw", position: "relative" }}>
+      <div className="row d-flex justify-content-between" style={{
+        height: "100%",
+        width: "100%",
+        position: "absolute",
+        top: "0",
+      }}>
+        <div id="left-bar" className="col-3 shadow-lg" align="center" style={{
+          backgroundColor: "#fff",
+          zIndex: "2",
+          paddingTop: "5rem",
+          height: "100%",
+          position: "relative",
+        }}>
+          <div
+            className="text-white"
+            style={{ zIndex: "4", position: "relative" }}
+          >
+
+            <h3>Select a region:</h3>
+            <select
+              id="region-selection"
+              class="form-select"
+              onChange={processRegionSelection}
+            >
+              <option value="" defaultValue hidden>
+                Select a region
+              </option>
+              <option value="region_71">71</option>
+              <option value="region_70">70</option>
+              <option value="region_311">311</option>
+            </select>
+
+          </div>
+          <div className="bg-primary region_selection_banner">
+            
+          </div>
+
+        </div>
+      </div>
+      <ReactMapGL
+        {...viewport}
+        width="100%"
+        height="100%"
+        onViewportChange={setViewport}
+        mapboxApiAccessToken={
+          "pk.eyJ1IjoieGxpdHRvYm95eHgiLCJhIjoiY2tscHFmejN4MG5veTJvbGhyZjFoMjR5MiJ9.XlWX6UhL_3qDIlHl0eUuiw"
+        }
+        
+      >
+        
+
+        
+      </ReactMapGL>
     </div>
   );
 }
