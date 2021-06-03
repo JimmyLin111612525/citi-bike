c967f5a39fe5897d45efcca9524acd1794a987e9
jimmy <jimmy.lin@stonybrook.edu>
2021-06-02 20:10:02 -0400

    Added sorting, bike icon, and zoom in on click.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import ReactMapGL, { Marker } from "react-map-gl";
 import React, { useState } from "react";
+import { Bicycle } from 'react-bootstrap-icons';
 
 const App = () => {
   const [stationLayers, setStationLayers] = useState(null)
@@ -23,6 +24,11 @@ const App = () => {
 
   const processRegionSelection = (e) => {
     console.log('process')
+    let radios = document.getElementsByClassName("form-check-input")
+    // console.log(radios)
+    for (let r of radios) {
+      r.checked = false
+    }
     const regionNumber = parseInt(e.target.value.split("_")[1])
     const stationsOBJ = Object.values(stationsJSON)
     let stationCards = []
@@ -35,6 +41,35 @@ const App = () => {
         stationCards.push(newStation)
 
       }
+    }
+
+    if (regionNumber == 70) {
+      setViewport((prevViewport) => {
+        return {
+          ...prevViewport,
+          longitude: -74.05474749737286,
+          latitude: 40.727072570161255,
+          zoom: 13.5
+        }
+      })
+    } else if (regionNumber == 311) {
+      setViewport((prevViewport) => {
+        return {
+          ...prevViewport,
+          latitude: 40.74499282902128,
+          longitude: -74.03353265304999,
+          zoom: 14.5
+        }
+      })
+    } else {
+      setViewport((prevViewport) => {
+        return {
+          ...prevViewport,
+          longitude: -73.94618117623384,
+          latitude: 40.75343397110906,
+          zoom:10.75
+        }
+      })
     }
 
     setStationCards(stationCards)
@@ -77,6 +112,15 @@ const App = () => {
           setChosenStation(newStat)
           setChosenCard(e.currentTarget)
           console.log(`Longitude : ${long}, Latitude : ${lat}`)
+          setViewport((prevViewport) => {
+            return {
+              ...prevViewport,
+              latitude: station.longitude,
+              longitude: station.latitude,
+              zoom: 16
+            };
+
+          })
         } else if (chosenCard && chosenCard.className.includes(e.currentTarget.className)) {
           console.log('here')
           chosenCard.classList.remove("bg-primary")
@@ -95,18 +139,19 @@ const App = () => {
           setChosenStation(newStat)
           setChosenCard(e.currentTarget)
           console.log(`Longitude : ${long}, Latitude : ${lat}`)
+          setViewport((prevViewport) => {
+            return {
+              ...prevViewport,
+              latitude: station.longitude,
+              longitude: station.latitude,
+              zoom: 16
+            };
+
+          })
         }
         // console.log(station.station_name)
 
-        setViewport((prevViewport) => {
-          return {
-            ...prevViewport,
-            latitude: station.longitude,
-            longitude: station.latitude,
-            zoom: 16
-          };
 
-        })
         break
       }
     }
@@ -116,21 +161,24 @@ const App = () => {
 
   const markerClicked = (e) => {
     e.preventDefault();
-    const index = parseInt(e.target.id.split("-")[1])
+    const index = parseInt(e.currentTarget.id.split("-")[1])
     let card = document.getElementById(`station-card-${index}`)
-    console.log(index)
-    if (chosenStation && chosenStation !== e.target) {
+    let container = document.getElementById('cards-container')
+    console.log(card.offsetHeight)
+    // console.log(index)
+    if (chosenStation && chosenStation !== e.currentTarget) {
       chosenStation.classList.remove('btn-danger')
       chosenStation.classList.add('btn-primary')
-      e.target.classList.remove('btn-primary')
-      e.target.classList.add('btn-danger')
+      e.currentTarget.classList.remove('btn-primary')
+      e.currentTarget.classList.add('btn-danger')
       chosenCard.classList.remove('bg-primary')
       chosenCard.classList.remove('text-white')
       card.classList.add("bg-primary")
       card.classList.add("text-white")
       setChosenCard(card)
-      setChosenStation(e.target);
-    } else if (chosenStation === e.target) {
+      setChosenStation(e.currentTarget);
+      container.scrollTop = card.offsetTop - (3 * card.offsetHeight);
+    } else if (chosenStation === e.currentTarget) {
       chosenStation.classList.remove('btn-danger')
       chosenStation.classList.add('btn-primary')
       card.classList.remove("bg-primary")
@@ -139,12 +187,13 @@ const App = () => {
       setChosenCard(null)
     }
     else {
-      e.target.classList.remove('btn-primary')
-      e.target.classList.add('btn-danger')
+      e.currentTarget.classList.remove('btn-primary')
+      e.currentTarget.classList.add('btn-danger')
       card.classList.add("bg-primary")
       card.classList.add("text-white")
-      setChosenStation(e.target);
+      setChosenStation(e.currentTarget);
       setChosenCard(card)
+      container.scrollTop = card.offsetTop - (3 * card.offsetHeight);
     }
 
 
@@ -152,6 +201,7 @@ const App = () => {
   }
 
   const mapClicked = (e) => {
+    console.log(e.lngLat)
     console.log(stationCards.length)
     console.log("marker clicked")
     console.log(chosenStation)
@@ -167,25 +217,38 @@ const App = () => {
     setChosenCard(null)
     e.stopPropagation();
 
+  }
 
-    // if(e.target.classList.contains('chosen')){
-    //   console.log('already chosen')
-    //   e.target.classList.remove('chosen')
-    //   console.log(chosenStation)
-    // }else{
-    //   setChosenStation(130)
-    //   e.target.classList.add('chosen')
-    // }
+  const sortCardsDescending = () => {
+    let tempCards = stationCards;
+    console.log(tempCards)
+    tempCards.sort((a, b) => {
+      if (a.visits - b.visits > 0) {
+        return -1;
+      } else if (a.visits - b.visits < 0) {
+        return 1;
+      } else {
+        return 0;
+      }
+    })
+    console.log(tempCards)
+    setStationCards([...tempCards])
+  }
 
-    // if (chosenStation === null) {
-    //   console.log('not chosen')
-    //   setChosenStation(3)
-    //   console.log(chosenStation)
-    // } else {
-    //   console.log('chosen')
-    //   console.log(chosenStation)
-    // }
-
+  const sortCardsAscending = () => {
+    let tempCards = stationCards;
+    console.log(tempCards)
+    tempCards.sort((a, b) => {
+      if (a.visits - b.visits > 0) {
+        return 1;
+      } else if (a.visits - b.visits < 0) {
+        return -1;
+      } else {
+        return 0;
+      }
+    })
+    console.log(tempCards)
+    setStationCards([...tempCards])
   }
 
   return (
@@ -230,11 +293,27 @@ const App = () => {
           {stationCards ? (
             <div
               className="d-flex flex-column justify-content-between"
-              style={{ height: "90%", width: "100%" }}
+              style={{ height: "85%", width: "100%" }}
             >
               <hr></hr>
-              <h5>Choose a station:</h5>
+              <h5 style={{ paddingTop: "20px" }}>Choose a station:</h5>
+              <div className="flex-row d-flex mx-6">
+                Sort by:
+                <div className="col form-check" style={{ marginLeft: "10px" }}>
+                  <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" onClick={sortCardsDescending} />
+                  <label className="form-check-label" htmlFor="flexRadioDefault1">
+                    Most Visited
+                  </label>
+                </div>
+                <div className="col form-check" style={{ marginLeft: "10px" }}>
+                  <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" onClick={sortCardsAscending} />
+                  <label className="form-check-label" htmlFor="flexRadioDefault2">
+                    Least Visited
+                  </label>
+                </div>
+              </div>
               <div
+                id="cards-container"
                 style={{ overflow: "auto", height: "85%" }}
               >
                 {stationCards.map((station, index) => {
@@ -276,7 +355,11 @@ const App = () => {
             stationCards.map((station, index) => {
               return (
                 <Marker key={station.station_name} latitude={station.longitude} longitude={station.latitude} >
-                  <button className={`btn btn-primary`} id={`marker-${index}`} onClick={markerClicked}></button>
+                  <button className={`shadow btn btn-primary border border-white p-0`} id={`marker-${index}`} onClick={markerClicked}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="25" fill="currentColor" className="bi bi-bicycle" viewBox="0 0 16 16">
+                      <path d="M4 4.5a.5.5 0 0 1 .5-.5H6a.5.5 0 0 1 0 1v.5h4.14l.386-1.158A.5.5 0 0 1 11 4h1a.5.5 0 0 1 0 1h-.64l-.311.935.807 1.29a3 3 0 1 1-.848.53l-.508-.812-2.076 3.322A.5.5 0 0 1 8 10.5H5.959a3 3 0 1 1-1.815-3.274L5 5.856V5h-.5a.5.5 0 0 1-.5-.5zm1.5 2.443-.508.814c.5.444.85 1.054.967 1.743h1.139L5.5 6.943zM8 9.057 9.598 6.5H6.402L8 9.057zM4.937 9.5a1.997 1.997 0 0 0-.487-.877l-.548.877h1.035zM3.603 8.092A2 2 0 1 0 4.937 10.5H3a.5.5 0 0 1-.424-.765l1.027-1.643zm7.947.53a2 2 0 1 0 .848-.53l1.026 1.643a.5.5 0 1 1-.848.53L11.55 8.623z" />
+                    </svg>
+                  </button>
                 </Marker>
               )
             })
